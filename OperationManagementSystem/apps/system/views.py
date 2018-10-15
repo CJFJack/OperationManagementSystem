0bb48b12950b1c8dcba21747f6e7fc6eee788ea3
jack_chen <jack_chen@shinetour.com>
2018-10-15 22:27:38 +0800

    sync all ecs from acs

--- OperationManagementSystem/apps/system/views.py
+++ OperationManagementSystem/apps/system/views.py
@@ -9,6 +9,7 @@ from django.utils.decorators import method_decorator
 from acs_api.acs_sync_all_ecs import sync_all_ecs
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 import json
 
@@ -26,20 +27,21 @@ class ECSListView(generic.ListView):
 
 
 @login_required(login_url='/login/')
+@csrf_exempt
 def sync_all_ecs_from_acs(request):
     ecs_acs = sync_all_ecs(region_id='cn-hangzhou')
     ecs_local = []
     '''获取本地所有ecs实例id的list'''
     for ecs in ECS.objects.all():
-        ecs_local.append(ecs.instanceid)
+        ecs_local.append(ecs.instance_id)
     '''增加本地没有的ecs'''
     for ecs_instance_id in ecs_acs:
         if ecs_instance_id not in ecs_local:
-            ecs = ECS(instanceid=ecs_instance_id)
+            ecs = ECS(instance_id=ecs_instance_id)
             ecs.save()
     '''删除阿里云没有的本地ecs'''
     for ecs_instance_id in ecs_local:
         if ecs_instance_id not in ecs_acs:
-            ecs = get_object_or_404(ECS, instanceid=ecs_instance_id)
+            ecs = get_object_or_404(ECS, instance_id=ecs_instance_id)
             ecs.delete()
     return HttpResponse(json.dumps({'success': True}), content_type="application/json")
