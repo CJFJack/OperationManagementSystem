ff3b4bfb6c41502510fb08fd609e7988dd246038
CJFJack <398741302@qq.com>
2018-12-30 23:44:46 +0800

    add block jenkins api test

--- OperationManagementSystem/apps/jenkins/tests.py
+++ OperationManagementSystem/apps/jenkins/tests.py
@@ -3,4 +3,34 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
-# Create your tests here.
+from jenkinsapi.jenkins import Jenkins
+from jenkinsapi.utils.crumb_requester import CrumbRequester
+
+jenkins_url = 'http://127.0.0.1:8080/'
+username = 'admin'
+password = 'Python@123'
+jenkins = Jenkins(jenkins_url, username=username, password=password,
+                  requester=CrumbRequester(
+                      baseurl=jenkins_url,
+                      username=username,
+                      password=password,
+                  ))
+
+params = {'min': 1}
+
+# This will start the job in non-blocking manner
+# jenkins.build_job('test-job', params)
+
+
+# This will start the job and will return a QueueItem object which
+# can be used to get build results
+job = jenkins['test-job']
+qi = job.invoke(build_params=params)
+
+# Block this script until build is finished
+if qi.is_queued() or qi.is_running():
+    qi.block_until_complete()
+
+build = qi.get_build()
+print build
+print build._data['result']
